45cb11fb645393ad7b633a5cc5c9014064eed968
Joshua Galvez <joshuagalvez119@gmail.com>
2021-05-27 00:32:19 -0700

    moment js and page display color based on time

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('hello');
+var today = moment();
+var hour = today.format('H');
+
+var timeBlocks = $(".container");
+$("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
+
+
+if(hour < 9){
+    for(var i = 0; i < timeBlocks.children().length; i++){
+        timeBlocks.children().eq(i).children().eq(1).addClass('future');
+    }
+}else if(hour > 17){
+    for(var i = 0; i < timeBlocks.children().length; i++){
+        timeBlocks.children().eq(i).children().eq(1).addClass('past');
+    }
+}else{
+    for(var i = 0; i < timeBlocks.children().length; i++){
+        if(hour - 9 > i){
+            timeBlocks.children().eq(i).children().eq(1).addClass('past');
+        }else if(hour - 9 == i){
+            timeBlocks.children().eq(i).children().eq(1).addClass('present');
+        }else{
+            timeBlocks.children().eq(i).children().eq(1).addClass('future');
+        }
+    }
+}
+
