e80e731faeaacce2741cb27ea5c1cb1e495c2d37
Joshua Galvez <joshuagalvez119@gmail.com>
2021-05-27 02:38:21 -0700

    local storage and display function

---
+++ README.md
@@ -0,0 +1,3 @@
+Responsive Day planner that clearly shows the progress of your day and allows you to plan for day and save the results to local storage so it will not be lost.
+
+![ssd](./assets/daySchedulerCapture.PNG)
--- script.js
+++ script.js
@@ -4,6 +4,12 @@ var hour = today.format('H');
 var timeBlocks = $(".container");
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
 
+if(localStorage.getItem("plans")){
+    plans = JSON.parse(localStorage.getItem("plans"));
+}else{
+    var plans = ['', '', '', '', '', '', '', '', ''];
+}
+displayPlans();
 
 if(hour < 9){
     for(var i = 0; i < timeBlocks.children().length; i++){
@@ -25,3 +31,24 @@ if(hour < 9){
     }
 }
 
+function displayPlans(){
+    for(var i = 0; i < plans.length; i++){
+        timeBlocks.children().eq(i).children().eq(1).attr('value', plans[i]);
+        console.log(timeBlocks.children().eq(i).children().eq(1));
+    }
+}
+
+
+var save = $(".saveBtn");
+
+for(var i = 0; i < save.length; i++){
+    console.log('hi');
+    save[i].addEventListener('click', function () {
+        var btnNum = $(this).parent().index();
+        plans[btnNum] = $(this).siblings('input').val();
+        console.log(plans);
+        console.log($(this).siblings('input').val());
+        console.log(btnNum);
+        localStorage.setItem('plans', JSON.stringify(plans));
+    });
+}
